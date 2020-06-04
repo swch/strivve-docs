c588b9c7997f78d70a9013d1f5913ab0ceede0bf
Hopen <chopen@switchme.com>
2020-06-03 18:47:33 -0700

    Fixes for swagger rendering

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,36 +14,3 @@ exports.onCreatePage = async ({ page, actions }) => {
     createPage(page)
   }
 };
-
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
-
-exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
-  return new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allOpenApiSpec {
-          edges {
-            node {
-              id
-              name
-            }
-          }
-        }
-      }
-    `).then(result => {
-      result.data.allOpenApiSpec.edges.map(({ node }) => {
-        createPage({
-          path: `apis/${node.name}`,
-          component: path.resolve(`./src/templates/swagger.js`),
-          context: {
-            id: node.id,
-          },
-        })
-      })
-
-      resolve()
-    })
-  })
-}
--- src/templates/swagger.js
+++ src/templates/swagger.js
@@ -1,15 +1,19 @@
 // Swagger template
 // https://github.com/devdigital/gatsby-source-openapi-aggregate
 import React, { Component } from 'react'
+import _ from 'lodash';
+
+
 import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
 import { graphql } from "gatsby"
+import {Layout} from '../components/index';
+import DocsMenu from '../components/DocsMenu';
+import {htmlToReact, getPages, Link, safePrefix} from '../utils';
 
 //import Specs from '../swagger-spec/Specs'
 import SpecPaths from '../swagger-spec/SpecPaths'
 import SpecInformation from '../swagger-spec/SpecInformation'
 import groupBy from 'lodash.groupby'
-import g from 'glamorous'
 
 const backStyle = {
   marginBottom: '1rem',
@@ -22,21 +26,43 @@ class Api extends Component {
     const paths = api.childrenOpenApiSpecPath
     const pathGroups = groupBy(paths, p => p.tag)
 
+    let root_page_path = _.get(this.props, 'pageContext.site.data.doc_sections.root_folder') + 'index.md';
+    let current_page_path = '/' + _.get(this.props, 'pageContext.relativePath');
+    let child_pages_path = '/' + _.get(this.props, 'pageContext.relativeDir');
+    let child_pages = _.orderBy(_.filter(getPages(this.props.pageContext.pages, child_pages_path), item => _.get(item, 'base') !== 'index.md'), 'frontmatter.weight');
+    let child_count = _.size(child_pages);
+    let has_children = (child_count > 0) ? true : false;
     return (
-      <div>
-        <g.Div css={backStyle}>
-          <Link to="/">Back</Link>
-        </g.Div>
-        <SpecInformation
-          title={api.title}
-          version={api.version}
-          description={api.description}
-        />
-        {Object.keys(pathGroups).map(t => (
-          <SpecPaths key={t} tag={t} paths={pathGroups[t]} />
-        ))}
-      </div>
-    )
+        <Layout {...this.props}>
+          <div className="outer">
+            <div className="inner">
+              <div className="docs-content">
+                <DocsMenu {...this.props} page={this.props.pageContext} site={this.props.pageContext.site} />
+                <article className="post type-docs">
+                  <div className="post-inside">
+                    <header className="post-header">
+                      <h1 className="post-title line-left">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
+                    </header>
+                    <div className="post-content">
+                      <div>
+                        <SpecInformation
+                          title={api.title}
+                          version={api.version}
+                          description={api.description}
+                        />
+                        {Object.keys(pathGroups).map(t => (
+                          <SpecPaths key={t} tag={t} paths={pathGroups[t]} />
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </div>
+        </Layout>
+    );
+
   }
 }
 
