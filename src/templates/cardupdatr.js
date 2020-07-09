4b5def4122d865c6630319f14596a7bbf9c04b94
Hopen <chopen@switchme.com>
2020-07-09 16:37:46 -0700

    Update cardupdatr.js

--- src/templates/cardupdatr.js
+++ src/templates/cardupdatr.js
@@ -16,52 +16,51 @@ export default class CardUpdatr extends React.Component {
 
     render() {
         if (isBrowser) {
-          window.initCardupdatr = function (app_container_id, hostname, username) {
-              const cardupdatr_app_container = document.getElementById(app_container_id);
-              // Create an iFrame to load in to the container.
-              const cardupdatr_iframe = document.createElement("iframe");
-              const stylesheet = document.styleSheets[0];
-              // Set up the required properties on the iFrame element (no scroll, transparency, border, id);
-              cardupdatr_iframe.id = "cardupdatr-app";
-              cardupdatr_iframe.style.border = "none";
-              cardupdatr_iframe.allowtransparency = "true";
-              cardupdatr_iframe.scroll = "no";
-              cardupdatr_iframe.style.margin = 0;
-              cardupdatr_iframe.style.padding = 0;
-              cardupdatr_iframe.style.minHeight = "100vh";
-              cardupdatr_iframe.style.width = "100%"
+            window.initCardupdatr = function (app_container_id, hostname, username) {
+                // Create an iFrame to load in to the container.
+                const cardupdatr_iframe = document.createElement("iframe");
+                // Set up the required properties on the iFrame element (no scroll, transparency, border, id);
+                cardupdatr_iframe.id = "cardupdatr-app";
+                cardupdatr_iframe.style.border = "none";
+                cardupdatr_iframe.allowtransparency = "true";
+                cardupdatr_iframe.scroll = "no";
+                cardupdatr_iframe.style.margin = 0;
+                cardupdatr_iframe.style.padding = 0;
+                cardupdatr_iframe.style.minHeight = "100vh";
+                cardupdatr_iframe.style.width = "100%";
 
-              // Set up the onload, which dynamically adjusts the height according to the document.
-              window.addEventListener('message', function (e) {
-                  var eventName = e.data[0];
-                  var data = e.data[1];
-                  switch (eventName) {
-                      case 'setHeight':
-                          cardupdatr_iframe.style.height = data + "px";
-                          break;
-                      case 'setTrace':
-                          console.log("set trace")
-                          window.location.hash = data;
-                  }
-              }, false);
-              // Append the iFrame to the supplied container div.
-              cardupdatr_app_container.appendChild(cardupdatr_iframe);
-              // Conditional loading of the source.
-              let app_source;
-              if (hostname) {
-                  app_source = hostname;
-              } else {
-                  app_source = "/"; // This should never happen based on the assumption that it'll be hosted on their domain. But for our own testing on staging/acme/etc. this will work.
-              }
-              if (username) {
-                  if (window.location.hash === "") {
-                      window.location.hash = "trace=" + username;
-                  } else {
-                      window.location.hash += "&trace=" + username;
-                  }
-              }
-              cardupdatr_iframe.src = app_source + "index.html#no-header&terms"; // Set the source of the iFrame
-          }
+                // Set up the onload, which dynamically adjusts the height according to the document.
+                window.addEventListener("message", function (e) {
+                    const eventName = e.data[0];
+                    const data = e.data[1];
+                    switch (eventName) {
+                        case "setHeight":
+                            cardupdatr_iframe.style.height = data + "px";
+                            break;
+                        case "setTrace":
+                            console.log("set trace");
+                            window.location.hash = data;
+                    }
+                }, false);
+                // Append the iFrame to the supplied container div.
+                const cardupdatr_app_container = document.getElementById(app_container_id);
+                cardupdatr_app_container.appendChild(cardupdatr_iframe);
+                // Conditional loading of the source.
+                let app_source;
+                if (hostname) {
+                    app_source = hostname;
+                } else {
+                    app_source = "/"; // This should never happen based on the assumption that it'll be hosted on their domain. But for our own testing on staging/acme/etc. this will work.
+                }
+                if (username) {
+                    if (window.location.hash === "") {
+                        window.location.hash = "trace=" + username;
+                    } else {
+                        window.location.hash += "&trace=" + username;
+                    }
+                }
+                cardupdatr_iframe.src = app_source + "index.html#no-header&terms"; // Set the source of the iFrame
+            };
         }
     return (
         <Layout {...this.props}>
