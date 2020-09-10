0af9e583cb1049b3c1bfa15ad3ee2ee4f745c4df
Mark Budos <markbudos@gmail.com>
2020-09-10 09:50:57 -0700

    Update cardupdatr-embed.js

--- src/templates/cardupdatr-embed.js
+++ src/templates/cardupdatr-embed.js
@@ -35,16 +35,21 @@ export default class CardUpdatr extends React.Component {
                         <header className="post-header">
                           <h1 className="post-title line-left">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
                         </header>                        
-                            <div className="post-content">
-                             {htmlToReact(_.get(this.props, 'pageContext.html'))}
-                            </div>
-                            </div>                                <div className="container" style={toStyleObj("background-color: deepskyblue; width: 100%; text-align: center; padding-top: 3vh; min-height: 100vh;")}>
-                                <ScriptTag src={cardupdatrJavascriptUrl} />
-                                <div className={iFrameClassID} id={iFrameClassID}></div>
-
-                         </div>
-                        </article>
-                    </div>
+                        <div className="post-content">
+                          {htmlToReact(_.get(this.props, 'pageContext.html'))}
+                        </div>
+                      </div>
+                      <div className="container" style={toStyleObj("background-color: deepskyblue; width: 100%; text-align: center; padding-top: 3vh; min-height: 100vh;")}>
+                        <ScriptTag src={cardupdatrJavascriptUrl} />
+                        <div className={iFrameClassID} id={iFrameClassID}></div>
+                      </div>
+                    </article>
+                    <nav id="page-nav" className="page-nav">
+                      <div id="page-nav-inside" className="page-nav-inside sticky">
+                        <h2 className="page-nav-title">Jump to Section</h2>
+                      </div>
+                    </nav>
+                  </div>
                 </div>
               </div>
             </Layout>
